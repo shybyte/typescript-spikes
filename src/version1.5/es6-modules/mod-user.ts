dd4a7e8ee7e40bf3f6e140e1326eadeb18fac13a
shybyte <ein@volloeko.de>
2018-06-07 07:06:55 +0200

    Improved es6-modules example

--- src/version1.5/es6-modules/mod-user.ts
+++ src/version1.5/es6-modules/mod-user.ts
@@ -1,5 +1,4 @@
-import DefaultModGreeter from './default-mod';
-import {greet} from './default-mod';
+import DefaultModGreeter, {greet} from './default-mod';
 import {myFunction, myOtherFunction as renamedOtherFunction, publicName} from './mod1';
 import * as mod1 from './mod1';
 import {function1, renamedFunction} from './re-export';
